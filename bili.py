e6d2f858036569dcbee19ab8c7f00b2a36aa358d
DuckBurnIncense <87637270+DuckBurnIncense@users.noreply.github.com>
2022-04-07 01:05:33 +0800

    Update
    支持批量处理

--- bili.py
+++ bili.py
@@ -1,18 +1,20 @@
 import sys
 
-if(len(sys.argv) != 2):
-    exit()
+if(len(sys.argv) < 2):
+    sys.exit()
 
-filePath = sys.argv[1]
+for a in sys.argv:
+	if(a != sys.argv[0]):
+		filePath = a
 
-pfile = open(filePath ,'rb')
-pfile.seek(3)
-byte = pfile.read()
-pfile.close()
+		pfile = open(filePath ,'rb')
+		pfile.seek(3)
+		byte = pfile.read()
+		pfile.close()
 
-pfile = open(filePath ,'wb')
-pfile.seek(0)
-pfile.write(byte)
-pfile.close()
+		pfile = open(filePath ,'wb')
+		pfile.seek(0)
+		pfile.write(byte)
+		pfile.close()
 
 print('success')
